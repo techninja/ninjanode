c76f69ea39ba91263ddd288aac6747fd53bb0547
techninja <james@tn42.com>
2013-10-29 13:24:45 +0000

    Shrink size of play area by a power of 10. Should make gameplay a little more interesting

--- ninjaroot/ninjaships.node.js
+++ ninjaroot/ninjaships.node.js
@@ -5,7 +5,7 @@
 
 // all Ships are held here with the key as the user hash
 var _ships = {};
-var _playArea = 20000 // Size of Square where users will wrap to other side
+var _playArea = 2000 // Size of Square where users will wrap to other side
 
 var projectileTypes = {
   laser: {
