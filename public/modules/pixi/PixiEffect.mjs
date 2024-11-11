315d52b92a866f49089e7029bcb70ca9e1bb3606
James Todd <james@tn42.com>
2024-11-11 12:08:32 -0800

    Add automated effect emitter culling
    Watches to see if it's inactive and has no particles left, then destroys, and recreates on next activation.

--- public/modules/pixi/PixiEffect.mjs
+++ public/modules/pixi/PixiEffect.mjs
@@ -20,38 +20,54 @@ export class PixiEffect {
   effect;
   container;
   emitter;
-  active;
+  active = false;
   started;
+  type;
   pos = { x: 0, y: 0, d: 0 };
 
-  constructor({ app, type, parent, pos, active = false }) {
+  constructor({ app, type, parent, pos = {}, active = false }) {
+    this.type = type;
+    this.pos = { ...this.pos, ...pos };
     this.started = Date.now();
     this.container = new Container();
     parent.addChild(this.container);
 
     this.loadAssets().then(() => {
-      switch (type) {
-        case 'explosion':
-          this.emitter = new Emitter(this.container, emitters.poof);
-          break;
-        case 'thruster':
-          this.emitter = new Emitter(this.container, emitters.thrust);
-          break;
-
-        default:
-          break;
-      }
-
       this.setActive(active);
 
-      // Set position.
-      this.emitter.resetPositionTracking();
-      this.setPos(pos || this.pos);
-
       app.ticker.add(() => {
-        this.emitter.update((Date.now() - this.started) * 0.00001);
+        if (this.emitter) {
+          this.emitter.update((Date.now() - this.started) * 0.00001);
+
+          // Cull inactive and empty emitters.
+          if (this.emitter.particleCount === 0 && !this.active) {
+            this.destroyEmitter();
+          }
+        }
       });
     });
+  }
+
+  destroyEmitter() {
+    if (this.emitter) {
+      this.emitter.destroy();
+      this.emitter = null;
+    }
+  }
+
+  initEmitter(type = this.type) {
+    this.destroyEmitter();
+    switch (type) {
+      case 'explosion':
+        this.emitter = new Emitter(this.container, emitters.poof);
+        break;
+      case 'thruster':
+        this.emitter = new Emitter(this.container, emitters.thrust);
+        break;
+
+      default:
+        break;
+    }
   }
 
   async loadAssets() {
@@ -73,6 +89,16 @@ export class PixiEffect {
 
   setActive(state) {
     this.active = !!state;
+
+    // Reinitialize culled emitters.
+    if (!this.emitter && state) {
+      this.initEmitter();
+
+      // Reset emitter position
+      this.emitter.resetPositionTracking();
+      this.setPos(this.pos);
+    }
+
     if (this.emitter) this.emitter.emit = !!state;
   }
 
