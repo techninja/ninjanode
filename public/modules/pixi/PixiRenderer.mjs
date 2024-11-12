9a2be1f18a15d572b474f0a726d99a0330b125c7
James Todd <james@tn42.com>
2024-11-12 14:22:50 -0800

    WIP background and star parallax
    Also gets game config from server

--- public/modules/pixi/PixiRenderer.mjs
+++ public/modules/pixi/PixiRenderer.mjs
@@ -5,20 +5,22 @@
 
 // Assume PIXI global namespace.
 // eslint-disable-next-line no-undef
-const { Application } = PIXI;
+const { Application, Container, Assets, TilingSprite } = PIXI;
 import { PixiShip, PixiCamera } from 'pixirender';
 
 export class PixiRenderer {
   app;
   socket;
   camera;
-  stage;
+  stage = {};
+  background;
 
   // ID keyed object of ship render info and elements.
   ships = {};
   projectiles = {};
   powerUps = {};
   pnbits = {};
+  gameConfig = {};
 
   constructor(options) {
     this.socket = options.socket;
@@ -28,16 +30,31 @@ export class PixiRenderer {
 
     const pixiOpts = { canvas, resizeTo: window };
     this.app.init(pixiOpts).then(async () => {
-      // Setup camera and stage where things are drawn onto the stage.
-      this.camera = new PixiCamera(this.app);
-      this.stage = this.camera.getStage();
+      // Before anything else, create a container for ships and bind events.
+      // If you take too long to do this, you will miss initial ship data.
+      this.stage.ships = new Container();
+      this.bindUpdateEvents();
 
+      // Setup camera and stage where layer containers are held and then added to.
+      this.gameConfig = await (await fetch('/game')).json();
+      this.camera = new PixiCamera(this.app, this.gameConfig);
+      this.stage.base = this.camera.getStage();
+
+      // Layers (including ships)
+      this.stage.background = new Container();
+
+      // Add the working stage "layers" to the base stage.
+      this.stage.base.addChild(this.stage.background);
+      this.stage.base.addChild(this.stage.ships);
+
+      // Setup the background.
+      this.initBackground();
+
+      // Minimum for creating a ship object.
       // window.ship = new PixiShip(this.app, {
       //   style: 'a',
       //   pos: { x: 150, y: 150, d: 0, t: 1 },
       // });
-
-      this.bindUpdateEvents();
     });
   }
 
@@ -49,7 +66,7 @@ export class PixiRenderer {
       switch (update.status) {
         case 'create':
           this.ships[id] = new PixiShip(this.app, {
-            parent: this.stage,
+            parent: this.stage.ships,
             ...update,
             onInit: () => {
               this.camera.follow(this.ships[id].container);
@@ -76,6 +93,40 @@ export class PixiRenderer {
           break;
       }
     }
+  }
+
+  async initBackground() {
+    // Load the tile texture
+    const { worldWidth, worldHeight } = this.stage.base;
+
+    const layers = [
+      new TilingSprite({
+        texture: await Assets.load('/resources/graphics/grid.png'),
+        width: worldWidth,
+        height: worldHeight,
+      }),
+      new TilingSprite({
+        texture: await Assets.load('/resources/graphics/starfield.png'),
+        width: worldWidth,
+        height: worldHeight,
+        scale: 2,
+      }),
+    ];
+
+    this.background = {
+      container: this.stage.background,
+      layers,
+    };
+
+    this.stage.background.addChild(layers[0]);
+    this.stage.background.addChild(layers[1]);
+
+    this.app.ticker.add(() => {
+      layers[1].tilePosition = {
+        x: -this.stage.base.center.x / 9,
+        y: -this.stage.base.center.y / 9,
+      };
+    });
   }
 
   onUpdateShipPos(posUpdates) {
