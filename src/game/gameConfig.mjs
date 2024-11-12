2211c4e891c8d574acb354a8c043967ed35dea40
James Todd <james@tn42.com>
2024-11-12 14:08:19 -0800

    Force env config to integers

--- src/game/gameConfig.mjs
+++ src/game/gameConfig.mjs
@@ -1,7 +1,7 @@
 const { env } = process;
 
 export const gameConfig = {
-  pnbitsCount: env['NINJANODE_PLANET_COUNT'] ?? 5,
-  powerupCount: env['NINJANODE_POWERUP_COUNT'] ?? 20,
-  playArea: env['NINJANODE_PLAY_SIZE'] ?? 20 * 1000,
+  pnbitsCount: parseInt(env['NINJANODE_PLANET_COUNT'], 10) ?? 5,
+  powerupCount: parseInt(env['NINJANODE_POWERUP_COUNT'], 10) ?? 20,
+  playArea: parseInt(env['NINJANODE_PLAY_SIZE'], 10) ?? 20 * 1000,
 };
