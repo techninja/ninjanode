d7c2794bceef9d5bbdf5a111763dc9d85999e45e
James Todd <james@tn42.com>
2024-11-24 21:28:10 -0800

    Export slide elements

--- public/modules/ui/components/elements/index.mjs
+++ public/modules/ui/components/elements/index.mjs
@@ -5,3 +5,5 @@ export * from './NinjaButton.mjs';
 export * from './NinjaIcon.mjs';
 export * from './NinjaTab.mjs';
 export * from './NinjaTabs.mjs';
+export * from './NinjaSlides.mjs';
+export * from './NinjaSlide.mjs';
