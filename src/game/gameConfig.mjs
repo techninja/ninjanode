f23683f66725afc69f63d024a0c3dd0b6e4e27a1
James Todd <james@tn42.com>
2024-11-28 22:39:54 -0800

    Fix failed default value reading

--- src/game/gameConfig.mjs
+++ src/game/gameConfig.mjs
@@ -1,7 +1,7 @@
 const { env } = process;
 
 export const gameConfig = {
-  pnbitsCount: parseInt(env['NINJANODE_PLANET_COUNT'], 10) ?? 5,
-  powerupCount: parseInt(env['NINJANODE_POWERUP_COUNT'], 10) ?? 20,
-  playArea: parseInt(env['NINJANODE_PLAY_SIZE'], 10) ?? 20 * 1000,
+  pnbitsCount: parseInt(env['NINJANODE_PLANET_COUNT'] ?? 5, 10),
+  powerupCount: parseInt(env['NINJANODE_POWERUP_COUNT'] ?? 20, 10),
+  playArea: parseInt(env['NINJANODE_PLAY_SIZE'] ?? 20 * 1000, 10),
 };
