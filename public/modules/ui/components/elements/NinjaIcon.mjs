7fd58515fc207192c3c19cbfa0a1596313570b87
James Todd <james@tn42.com>
2024-11-30 14:04:51 -0800

    Move to font based icons
    Should fix build maybe

--- public/modules/ui/components/elements/NinjaIcon.mjs
+++ public/modules/ui/components/elements/NinjaIcon.mjs
@@ -2,42 +2,62 @@
  * @file Icon element definition.
  */
 import { html } from 'hybrids';
+import iconfont from '/icons/fonts/iconfont.css' with { type: 'css' };
 
 export const NinjaIcon = {
   tag: 'ninja-icon',
   name: '',
   size: 32,
   angle: 0,
-  dark: true,
+  color: 'black',
+  hoverColor: 'white',
   solid: false,
-  fullWidth: false,
   disabled: false,
 
-  render: ({ name, dark, fullWidth, disabled, solid, size, angle }) => {
-    const svg = `icons/icons/svg/${solid ? 'solid' : 'regular'}/${name}${solid ? '-solid' : ''}.svg`;
-    const src =
+  render: ({ name, color, hoverColor, disabled, solid, size, angle }) => {
+    const icon = `hn-${name}${solid ? '-solid' : ''}`;
+    const pxSize = `${size}px`;
+    const shipImage =
       name.substr(0, 4) == 'ship'
         ? `resources/graphics/ships/ship_${name.substr(-1)}.png`
-        : svg;
+        : false;
+
+    const iconClasses = {
+      hn: !shipImage,
+      ship: !!shipImage,
+      [icon]: !shipImage,
+    };
 
     return html`
       <style>
         :host {
           display: inline-block;
-          width: ${`${size}px`};
-          height: ${`${size}px`};
+          width: ${pxSize};
+          height: ${pxSize};
         }
-        img {
-          width: ${fullWidth ? '100%' : `${size}px`};
-          filter: ${dark ? 'invert(0)' : 'invert(1)'};
-          ${disabled && 'filter: invert(0.5) sepia(1) saturate(0) hue-rotate(175deg)'};
+        .icon i {
+          font-size: ${pxSize};
+          color: ${color};
+          ${disabled &&
+        'filter: invert(0.5) sepia(1) saturate(0) hue-rotate(175deg)'};
           transform: ${`rotate(${angle}deg)`};
         }
+        .icon i:hover {
+          color: ${hoverColor};
+        }
+        .ship {
+          display: block;
+          content: ' ';
+          background-image: ${`url(${shipImage})`};
+          background-repeat: no-repeat;
+          background-size: contain;
+          width: ${pxSize};
+          height: ${pxSize};
+        }
       </style>
-      <img
-        src=${src}
-      >
-      </img>
-    `;
+      <div class="icon">
+        <i class=${iconClasses}></i>
+      </div>
+    `.style(iconfont);
   },
 };
