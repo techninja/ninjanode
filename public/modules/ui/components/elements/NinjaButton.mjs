1bce8a16eb80d49b583959a85ba2f1ac624c870f
James Todd <james@tn42.com>
2024-12-11 20:40:10 -0800

    Beef up the button
    Adds room for pixel border, makes buttons tabbable, and cleans up styles

--- public/modules/ui/components/elements/NinjaButton.mjs
+++ public/modules/ui/components/elements/NinjaButton.mjs
@@ -13,6 +13,8 @@ export const NinjaButton = {
   active: false,
   disabled: false,
   borderSize: 2,
+  backgroundColor: 'grey',
+  hoverColor: 'white',
 
   // Icon prop drilled attributes.
   icon: '',
@@ -31,13 +33,16 @@ export const NinjaButton = {
     icon,
     solid,
     angle,
+    backgroundColor,
+    hoverColor,
+    buttonIndex,
   }) => {
-    const linkClasses = {
+    const buttonClasses = {
       button: true,
       'is-active': active,
       'is-loading': loading,
     };
-    if (type) linkClasses[`is-${type}`] = true;
+    if (type) buttonClasses[`is-${type}`] = true;
 
     const buttonStyle = { display: fullWidth ? 'flex' : 'inline-block' };
 
@@ -45,32 +50,41 @@ export const NinjaButton = {
       <style>
         :host {
           display: inline-block;
-          cursor: pointer;
+          cursor: ${disabled ? 'not-allowed' : 'pointer'};
+          margin: ${borderSize}px;
+          background-color: ${backgroundColor};
           box-shadow:
             -${borderSize}px 0 0 0 black,
             ${borderSize}px 0 0 0 black,
             0 -${borderSize}px 0 0 black,
             0 ${borderSize}px 0 0 black;
         }
-        a {
-          display: block !important;
-          padding: 0.25em;
+        :host button:hover {
+          background-color: ${hoverColor};
         }
-        a:hover {
-          background-color: gray;
+        button {
+          padding: 0.25em;
+          border: none;
+          cursor: ${disabled ? 'not-allowed' : 'pointer'};
+          width: 100%;
+          height: 100%;
+          font-family: 'Pixelify Sans', monospace;
+          overflow: hidden;
+          background-color: transparent;
         }
       </style>
-      <a
-        class="${linkClasses}"
+      <button
+        class="${buttonClasses}"
         disabled=${disabled}
         style=${buttonStyle}
         title="${desc}"
+        tabindex="0"
       >
         ${icon &&
         html`<ninja-icon name=${icon} solid=${solid} angle=${angle} />`}
         ${text && html`<span>${text}</span>`}
         <slot></slot>
-      </a>
+      </button>
     `;
   },
 };
