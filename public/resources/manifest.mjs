f4f037afe07fbda3328421651d58da21eb07019f
James Todd <james@tn42.com>
2024-12-14 12:37:01 -0800

    Who else hates boilerplate?

--- public/resources/manifest.mjs
+++ public/resources/manifest.mjs
@@ -1,57 +1,65 @@
+/**
+ * @file Manifest, listing all assets to be preloaded
+ * before we play the game. Don't add too much!
+ */
+
 const graphics = '/resources/graphics';
+const graphicBundleAssets = {
+  'ship-a': `${graphics}/ships/ship_a.png`,
+  'ship-b': `${graphics}/ships/ship_b.png`,
+  'ship-c': `${graphics}/ships/ship_c.png`,
+  'ship-d': `${graphics}/ships/ship_d.png`,
+  'ship-e': `${graphics}/ships/ship_e.png`,
+  'ship-f': `${graphics}/ships/ship_f.png`,
+  'ship-g': `${graphics}/ships/ship_g.png`,
 
-export const graphicBundleAssets = [
-  { alias: 'ship-a', src: `${graphics}/ships/ship_a.png` },
-  { alias: 'ship-b', src: `${graphics}/ships/ship_b.png` },
-  { alias: 'ship-c', src: `${graphics}/ships/ship_c.png` },
-  { alias: 'ship-d', src: `${graphics}/ships/ship_d.png` },
-  { alias: 'ship-e', src: `${graphics}/ships/ship_e.png` },
-  { alias: 'ship-f', src: `${graphics}/ships/ship_f.png` },
-  { alias: 'ship-g', src: `${graphics}/ships/ship_g.png` },
+  spark: `${graphics}/spark.png`,
+  starfield: `${graphics}/starfield.png`,
+  stars: `${graphics}/stars-green.png`,
+  smoke: `${graphics}/explosions/smoke.png`,
 
-  { alias: 'spark', src: `${graphics}/spark.png` },
-  { alias: 'starfield', src: `${graphics}/starfield.png` },
-  { alias: 'stars', src: `${graphics}/stars-green.png` },
-  { alias: 'smoke', src: `${graphics}/explosions/smoke.png` },
-
-  // TODO: We only need one gray image and then to tint it.
-  { alias: 'shield_blue', src: `${graphics}/shields/shield_blue.png` },
-  { alias: 'shield_green', src: `${graphics}/shields/shield_green.png` },
-  { alias: 'shield_orange', src: `${graphics}/shields/shield_orange.png` },
-  { alias: 'shield_pink', src: `${graphics}/shields/shield_pink.png` },
-  { alias: 'shield_purple', src: `${graphics}/shields/shield_purple.png` },
-  { alias: 'shield_red', src: `${graphics}/shields/shield_red.png` },
-  { alias: 'shield_yellow', src: `${graphics}/shields/shield_yellow.png` },
-];
+  // TODO: We really only need one gray image and then to tint it.
+  shield_blue: `${graphics}/shields/shield_blue.png`,
+  shield_green: `${graphics}/shields/shield_green.png`,
+  shield_orange: `${graphics}/shields/shield_orange.png`,
+  shield_pink: `${graphics}/shields/shield_pink.png`,
+  shield_purple: `${graphics}/shields/shield_purple.png`,
+  shield_red: `${graphics}/shields/shield_red.png`,
+  shield_yellow: `${graphics}/shields/shield_yellow.png`,
+};
 
 const audio = '/resources/audio';
-export const soundBundleAssets = [
-  { alias: 'boom', src: `${audio}/explosion.wav` },
-  { alias: 'beep', src: `${audio}/button-beep.wav` },
-  { alias: 'join', src: `${audio}/confirm.wav` },
-  { alias: 'thrust', src: `${audio}/thrust.wav` },
-  { alias: 'fire1', src: `${audio}/fire1.wav` },
-  { alias: 'fire2', src: `${audio}/fire2.wav` },
-  { alias: 'fire3', src: `${audio}/fire3.wav` },
-  { alias: 'fire4', src: `${audio}/fire4.wav` },
-  { alias: 'fire5', src: `${audio}/fire5.wav` },
-  { alias: 'hit1', src: `${audio}/hit1.wav` },
-  { alias: 'hit2', src: `${audio}/hit2.wav` },
-  { alias: 'spawnSet', src: `${audio}/spawn_set.wav` },
-  { alias: 'spawnUnset', src: `${audio}/spawn_unset.wav` },
-  { alias: 'mine', src: `${audio}/mine_boom.wav` },
-  { alias: 'warning', src: `${audio}/warning.wav` },
-];
+const soundBundleAssets = {
+  boom: `${audio}/explosion.wav`,
+  beep: `${audio}/button-beep.wav`,
+  join: `${audio}/confirm.wav`,
+  thrust: `${audio}/thrust.wav`,
+  fire1: `${audio}/fire1.wav`,
+  fire2: `${audio}/fire2.wav`,
+  fire3: `${audio}/fire3.wav`,
+  fire4: `${audio}/fire4.wav`,
+  fire5: `${audio}/fire5.wav`,
+  hit1: `${audio}/hit1.wav`,
+  hit2: `${audio}/hit2.wav`,
+  spawnSet: `${audio}/spawn_set.wav`,
+  spawnUnset: `${audio}/spawn_unset.wav`,
+  mine: `${audio}/mine_boom.wav`,
+  warning: `${audio}/warning.wav`,
+};
+
+// COnvert our shorthand format above to the PIXI Manifest format.
+const renderManifestAssets = (assets) =>
+  Object.entries(assets).map(([alias, src]) => ({ alias, src }));
 
 export const manifest = {
   bundles: [
     {
       name: 'graphics',
-      assets: graphicBundleAssets,
+      assets: renderManifestAssets(graphicBundleAssets),
     },
     {
       name: 'sounds',
-      assets: soundBundleAssets,
+      assets: renderManifestAssets(soundBundleAssets),
     },
   ],
 };
