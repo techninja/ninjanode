d6cfd2c2563796fb9f07e41721d1dc6cba5504bb
James Todd <james@tn42.com>
2025-01-04 17:48:16 -0800

    Fix sfxr layout

---
+++ public/data/sfxrSounds/index.mjs
@@ -0,0 +1,12 @@
+/**
+ * SFXR Sound definitions, loaded from JSON.
+ * @goto https://sfxr.me/ to edit these sounds dynamically.
+ */
+
+import thrustGen from './thrustGen.json' with { type: 'json' };
+import powerup from './powerup.json' with { type: 'json' };
+
+export const sfxrSounds = {
+  thrustGen,
+  powerup,
+};
