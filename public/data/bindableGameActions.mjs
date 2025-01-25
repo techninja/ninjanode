d3a4eba4ab02c54cd99fb19aec910e1a45e152c8
James Todd <james@tn42.com>
2025-01-24 16:33:54 -0800

    Add command and interface data
    For the window

---
+++ public/data/bindableGameActions.mjs
@@ -0,0 +1,56 @@
+export const bindableGameActions = {
+  u: {
+    name: 'Fwd Thrust',
+    icon: {
+      name: 'angle-up',
+    },
+  },
+  d: {
+    name: 'Rev Thrust',
+    icon: {
+      name: 'angle-down',
+    },
+  },
+  l: {
+    name: 'Turn Left',
+    icon: {
+      name: 'angle-left',
+    },
+  },
+  r: {
+    name: 'Turn Right',
+    icon: {
+      name: 'angle-right',
+    },
+  },
+  f: {
+    name: 'Primary Weapon',
+    icon: {
+      name: 'bolt',
+    },
+  },
+  s: {
+    name: 'Secondary Weapon',
+    icon: {
+      name: 'brightness-low',
+    },
+  },
+  b: {
+    name: 'Set Spawn',
+    icon: {
+      name: 'home',
+    },
+  },
+  w: {
+    name: 'Toggle Window',
+    icon: {
+      name: 'bars',
+    },
+  },
+  c: {
+    name: 'Toggle Chat',
+    icon: {
+      name: 'comment-dots',
+    },
+  },
+};
---
+++ public/data/bindableInterfaceDevices.mjs
@@ -0,0 +1,14 @@
+export const bindableInterfaceDevices = {
+  keyboard: {
+    name: 'Keyboard',
+    icon: 'keyboard',
+  },
+  gamepad: {
+    name: 'Gamepad',
+    icon: 'gamepad',
+  },
+  mouse: {
+    name: 'Mouse',
+    icon: 'mouse',
+  },
+};
